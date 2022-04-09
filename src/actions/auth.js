90a979241ded18dd3fc45cb245c82c9b3fd17565
AngelCruz2001 <angi_ed01@hotmail.com>
2022-04-09 00:47:31 -0500

    Connect with websockets in heroku changing fetch 3

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -65,8 +65,8 @@ export const authStartRegisterProfessional = (data, setIsLogin) => {
     return async (dispatch) => {
         // dispatch(authCheckingStart());
         const formData = new FormData();
-        const url = 'http://127.0.0.1:8000/api/professional/create/';
-        // const url = 'https://ayudat-backend.herokuapp.com/api/professional/create/';
+        // const url = 'http://127.0.0.1:8000/api/professional/create/';
+        const url = 'https://ayudat-backend.herokuapp.com/api/professional/create/';
         for (const name in data) {
             formData.append(name, data[name]);
         }
